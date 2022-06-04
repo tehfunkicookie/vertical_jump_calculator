9006bf14bcbdd6130b9a3d163a490a60cdd8a89a
tehfunkicookie <jerrymar@umich.edu>
2022-06-03 23:05:28 -0700

    updated vertical jump main.cpp with ability to write to file

--- Vertical/main.cpp
+++ Vertical/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iomanip>
 #include <cmath>
+#include <fstream>
 
 #define GRAVITY 9.81
 // units of gravity are m/s^2
@@ -15,6 +16,8 @@ T potentialEnergy(T height, T mass){
 
 // function declarations
 void display_menu();
+void display_previous_calculations();
+void output_to_file();
 double verticalToSquat(double height, double user_weight);
 double squatToVertical(double squat_weight, double user_weight);
 double poundToKg(double pound);
@@ -22,6 +25,9 @@ double kgToPound(double kg);
 double inchesToMeters(double inches);
 double metersToInches(double meters);
 double squat(double PE, double height);
+
+// global variables
+std::vector<std::vector<double>> record_of_calculations;
 
 int main(){
     
@@ -53,14 +59,16 @@ int main(){
     int choice;
     
     std::cin >> choice;
+    std::cout << std::endl;
     
-    while (choice !=3){
+//    std::vector<std::vector<double>> record_of_calculations;
+    while (choice !=5){
         if (choice==1){
             // enter in function for vertical jump based on input of squat weight
             double user_squat_weight;
             double user_weight;
             
-            std::cout << "Please enter in how much you squat or want to squat in lbs: ";
+            std::cout << "Please enter in how much you squat in lbs: ";
             std::cin >> user_squat_weight;
             
             std::cout << "Please enter in how much you weigh in lbs: ";
@@ -68,9 +76,13 @@ int main(){
             
             double desired_vertical = squatToVertical(user_squat_weight,user_weight);
             
+            std::cout << std::endl;
             std::cout << "If you squat " << user_squat_weight << " lbs, you can have a standing vertical jump of " << desired_vertical << " inches.\n" << std::endl;
+            // store the calculation into record of calculations vector 
             
-        } else { // choice==2
+            record_of_calculations.push_back({1,user_squat_weight,desired_vertical});
+            
+        } else if (choice == 2) { // choice==2
             // enter in function for squat weight based on vertical jump
             double user_vertical;
             double user_weight;
@@ -83,12 +95,20 @@ int main(){
             
             double desired_squat = verticalToSquat(user_vertical,user_weight);
             
+            std::cout << std::endl;
             std::cout << "If you can or want to jump " << user_vertical << " inches, you have to squat at least " << desired_squat << " lbs.\n" << std::endl;          
 
+            record_of_calculations.push_back({2,user_vertical,desired_squat});
+            
+        } else if (choice == 3){
+            display_previous_calculations();
+        } else if (choice ==4){
+            output_to_file();
         }
              
         display_menu();
         std::cin >> choice;
+        std::cout << std::endl;
     }
     
     
@@ -101,7 +121,39 @@ void display_menu(){
     std::cout << "Please select from the following: " << std::endl;
     std::cout << "1. Determine vertical jump height required from squat weight" << std::endl;
     std::cout << "2. Determine squat weight required for vertical jump height" << std::endl;
-    std::cout << "3. QUIT" << std::endl;
+    std::cout << "3. Print out previous calculations" << std::endl;
+    std::cout << "4. Send calculations out to text file" << std::endl;
+    std::cout << "5. QUIT" << std::endl;
+}
+
+void display_previous_calculations(){
+    std::cout << std::setprecision(0) << std::fixed;
+    std::cout << "ID" << "                     " << "Calculation Description" << std::endl;
+    std::cout << "------------------------------------------------------------------------------------------------------------------" << std::endl;
+    for (int i=0;i<record_of_calculations.size();i++){
+        if (record_of_calculations[i][0]==1){
+            std::cout << i+1 << "                      " << "If you squat " << record_of_calculations[i][1] << " lbs, you can have a standing vertical jump of " <<  record_of_calculations[i][2] << " inches.\n" << std::endl;
+        } else if (record_of_calculations[i][0]==2){
+            std::cout << i+1 << "                      " << "If you can or want to jump " << record_of_calculations[i][1] << " inches, you have to squat at least " << record_of_calculations[i][2] << " lbs.\n" << std::endl;       
+        }
+    }
+}
+
+void output_to_file(){
+    std::cout << "Outputting previous calculations to external text file..." << std::endl;
+    std::ofstream outfile ("verticalJump_Calculations.txt");
+
+    outfile << std::setprecision(0) << std::fixed;
+    outfile << "ID" << "                     " << "Calculation Description" << std::endl;
+    outfile << "------------------------------------------------------------------------------------------------------------------" << std::endl;
+    for (int i=0;i<record_of_calculations.size();i++){
+        if (record_of_calculations[i][0]==1){
+            outfile << i+1 << "                      " << "If you squat " << record_of_calculations[i][1] << " lbs, you can have a standing vertical jump of " <<  record_of_calculations[i][2] << " inches.\n" << std::endl;
+        } else if (record_of_calculations[i][0]==2){
+            outfile << i+1 << "                      " << "If you can or want to jump " << record_of_calculations[i][1] << " inches, you have to squat at least " << record_of_calculations[i][2] << " lbs.\n" << std::endl;       
+        }
+    }
+    std::cout << std::endl;
 }
 
 double squatToVertical(double squat_weight, double user_weight){
